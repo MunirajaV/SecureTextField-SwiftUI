2023ed19dd0f93717e94dea679bd2cc8c3adb0f2
muniraja <muniraja@sparkouttech.com>
2020-08-25 20:40:40 +0530

    Initial Commit

---
+++ secureField/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  secureField
+//
+//  Created by Muniraja Velumani on 25/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
